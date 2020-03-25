ce98ae3fd64f81e70f46790e712d64dbe7f58e38
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-03-25 20:27:23 +0100

    add signin form validation

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -3,7 +3,13 @@ const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
-const { requireEmail, requirePassword, requirePasswordConfirm } = require('./validators');
+const {
+	requireEmail,
+	requirePassword,
+	requirePasswordConfirm,
+	requireEmailExist,
+	requireValidPasswordForUser
+} = require('./validators');
 
 const router = express.Router();
 
@@ -33,18 +39,13 @@ router.get('/signin', (req, res) => {
 	res.send(signinTemplate());
 });
 
-router.post('/signin', async (req, res) => {
-	const { email, password } = req.body;
+router.post('/signin', [ requireEmailExist, requireValidPasswordForUser ], async (req, res) => {
+	const errors = validationResult(req);
+	console.log(errors);
+
+	const { email } = req.body;
 	const user = await usersRepo.getOneBy({ email });
-	if (!user) {
-		return res.send('Email not found.');
-	}
 
-	const validPassword = await usersRepo.comparePassword(user.password, password);
-
-	if (!validPassword) {
-		return res.send('Invalid password.');
-	}
 	req.session.userId = user.id;
 	res.send('Welcome.');
 });
