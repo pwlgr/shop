d356360f0f1ddea8432d7b218c0ca3230d195a91
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-03-27 19:16:12 +0100

    add multer middleware to handle image uploads

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { validationResult } = require('express-validator');
+const multer = require('multer');
+
 const productsRepo = require('../../repositories/products');
 const prodcutsNewTemplate = require('../../views/admin/products/new');
 const { requirePrice, requireTitle } = require('./validators');
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/admin/products', (req, res) => {});
 
@@ -12,9 +15,9 @@ router.get('/admin/products/new', (req, res) => {
 	res.send(prodcutsNewTemplate({}));
 });
 
-router.post('/admin/products/new', [ requireTitle, requirePrice ], (req, res) => {
+router.post('/admin/products/new', [ requireTitle, requirePrice ], upload.single('image'), (req, res) => {
 	const errors = validationResult(req);
-	console.log(errors);
+	console.log(req.file);
 	res.send('submitted');
 });
 
