6daa98fba558fc544b108fa3c07c37c437ee39a8
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-03-29 19:08:05 +0200

    add deleting products from cart

--- views/carts/show.js
+++ views/carts/show.js
@@ -1,6 +1,11 @@
 const layout = require('../layout');
 
 module.exports = ({ items }) => {
+	let total = 0;
+	let totalPrice = items.reduce((prev, next) => {
+		return prev + next.quantity * next.product.price;
+	}, 0);
+
 	const renderedItems = items
 		.map((item) => {
 			return `
@@ -14,7 +19,8 @@ module.exports = ({ items }) => {
               $${item.product.price * item.quantity}
             </div>
             <div class="remove">
-              <form method="POST">
+              <form method="POST" action="/cart/products/delete">
+                <input hidden value="${item.id}" name="itemId" />
                 <button class="button is-danger">                  
                   <span class="icon is-small">
                     <i class="fas fa-times"></i>
@@ -42,7 +48,7 @@ module.exports = ({ items }) => {
               <div class="message-header">
                 Total
               </div>
-              <h1 class="title">$</h1>
+              <h1 class="title">${totalPrice}$</h1>
               <button class="button is-primary">Buy</button>
             </div>
           </div>
